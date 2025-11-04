fc5b71cdd29a13cf09d1b0ff441fb53cbf2f73f7
Komvopoulos <komvopoulos@seeders.gr>
2025-11-03 21:57:07 -0500

    Project Finalization

--- app.py
+++ app.py
@@ -9,34 +9,6 @@ memory = ConversationBufferMemory(memory_key="chat_history", return_messages=Tru
 CORS(app, origins=["http://localhost:8000", "http://127.0.0.1:8000"])
 app.secret_key = 'euroleague_chat'
 
-# def get_chatbot_response(user_message):
-
-#     print(f"Received message: {user_message}")
-
-#     return questioning(user_message)
-
-# @app.route("/chat", methods=["POST"])
-# def chat():
-
-#     try:
-#         data = request.get_json()
-#         user_message = data.get("message")
-
-#         if not user_message:
-#             return jsonify({"error": "No message provided"}), 400
-
-#         bot_response = get_chatbot_response(user_message)
-
-#         return jsonify({"reply": bot_response})
-
-#     except Exception as e:
-#         print(f"Error processing request: {e}")
-#         return jsonify({"error": "An internal error occurred"}), 500
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5000)
-
-
 @app.route("/chat", methods=["POST"])
 def chat():
     try:
@@ -73,4 +45,3 @@ def chat():
     except Exception as e:
         print(f"Error processing request: {e}")
         return jsonify({"error": "An internal error occurred"}), 500
-
